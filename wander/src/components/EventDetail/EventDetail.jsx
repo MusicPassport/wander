2e5c6e9b449b899ec833e91efee4bc74485105c7
sahirabibi <sahirabibi96@gmail.com>
2021-10-25 11:08:26 -0400

    Bug fix for event detail

--- wander/src/components/EventDetail/EventDetail.jsx
+++ wander/src/components/EventDetail/EventDetail.jsx
@@ -2,23 +2,21 @@ import { useState, useEffect, useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import { DataContext } from '../../Utility/Context';
 import axios from 'axios';
+import './EventDetail.css';
 
 function EventDetail() {
 	const { id } = useParams();
 	const { events, setEvents } = useContext(DataContext);
-	const [eventDetail, setEventDetail] = useState({...events.filter((event) => event.id === id)[0]});
+	const [eventDetail, setEventDetail] = useState();
 
-	
+	const url = `https://app.ticketmaster.com/discovery/v2/events/${id}.json?apikey=${'RW9cwwI0fopdanO8UIpgzYPYq0GlSavB'}`;
 
 	useEffect(() => {
-		setEventDetail({
-			...events.filter((event) => event.id === id)[0],
-		});
-	},[])
+		axios.get(url).then((res) => setEventDetail({ ...res.data }));
+	}, []);
 
-	
-	if (JSON.stringify(eventDetail) === '{}') {
-		return <h1>Loading...</h1>
+	if (!eventDetail) {
+		return <h1>Loading...</h1>;
 	}
 
 	//display the event detail in a card
@@ -27,25 +25,29 @@ function EventDetail() {
 			<div className='images-container'>
 				<img
 					className='event-img'
-					src={eventDetail.images[0].url}
+					src={eventDetail.images[1].url}
 					alt={`${eventDetail.name} + promo`}></img>
-			</div>
-			<div className='detail-btns'>
-				<button className='btn detail-btn bucket'>Add To BucketList</button>
-				<button className='btn detail-btn seen'>Add To Seen</button>
-				<a href={eventDetail.url}>
-					<button className='btn detail-btn tickets'>View Tickets</button>
-				</a>
 			</div>
 			<div className='info-container'>
 				<h2>{eventDetail.name}</h2>
-				{/* <p>Start Date: {eventDetail.start.localDate}</p> */}
-				<div className='seat-map'>
+				<p>Start Date: {eventDetail.dates.start.localDate}</p>
+				<div className='detail-btns'>
+					<button className='btn detail-btn bucket'>Add To BucketList</button>
+					<button className='btn detail-btn seen'>Add To Seen</button>
+					<a target='_blank' href={eventDetail.url}>
+						<button className='btn detail-btn tickets'>View Tickets</button>
+					</a>
+				</div>
+				{
+					eventDetail.seatmap ? 
+					<div className='seat-map'>
 					<img
 						className='event-img seat-img'
-						// src={eventDetail.seatmap.staticUrl}
+						src={eventDetail.seatmap.staticUrl}
 						alt={`${eventDetail.name} + seat map`}></img>
-				</div>
+				</div> : null
+				}
+	
 			</div>
 		</div>
 	);
