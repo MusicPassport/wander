e2aa434537b45b4fe05f5a8fac90caf049b7fc08
sahirabibi <sahirabibi96@gmail.com>
2021-10-25 12:26:55 -0400

    Add user bucketList and Seen component

--- wander/src/components/EventDetail/EventDetail.jsx
+++ wander/src/components/EventDetail/EventDetail.jsx
@@ -12,8 +12,77 @@ function EventDetail() {
 	const url = `https://app.ticketmaster.com/discovery/v2/events/${id}.json?apikey=${'RW9cwwI0fopdanO8UIpgzYPYq0GlSavB'}`;
 
 	useEffect(() => {
-		axios.get(url).then((res) => setEventDetail({ ...res.data }));
+		axios
+			.get(url)
+			.then((res) => setEventDetail({ ...res.data }))
+			.catch((err) => console.log(err));
 	}, []);
+
+	const getEvent = async () => {
+		try {
+			const event = await axios.get(
+				`https://intense-island-04626.herokuapp.com/events/${id}`
+			);
+			if (event.status === 200) {
+				return true;
+			} else if (event.status === 404) {
+				return false;
+			}
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
+	const formatData = async () => {
+		const newEvent = {
+			name: eventDetail.name,
+			genre: eventDetail.genre.name,
+			summary: eventDetail.promoter.description,
+			city: eventDetail.venues.city.name,
+			state: eventDetail.venues.state.name,
+			address: eventDetail.venues.address.line1,
+			tm_url: eventDetail.url,
+			img_url: eventDetail.images[0].url,
+			eventId: eventDetail.id,
+			start: eventDetail.dates.start.localDate,
+			venue: eventDetail.venues.name,
+			attendees: [],
+			viewers: [],
+		};
+		return newEvent;
+	}
+
+	const addSeen = () => {
+		// send a request to update the user detail to include the current user in the events attendees
+		if (getEvent) {
+			axios.put(`https://intense-island-04626.herokuapp.com/events${id}`, {
+				...formatData(),
+				attendees: 'currentUser',
+			});
+		} else {
+			axios.post(`https://intense-island-04626.herokuapp.com/events${id}`, {
+				...formatData(),
+				attendees: 'currentUser',
+			});
+
+		}
+		
+	};
+
+	const addBucketList = () => {
+		// send a request to update the user detail to include the current user in the events viewers
+		if (getEvent) {
+			axios.put(`https://intense-island-04626.herokuapp.com/events${id}`, {
+				...formatData(),
+				viewers: 'currentUser',
+			});
+		} else {
+			axios.post(`https://intense-island-04626.herokuapp.com/events${id}`, {
+				...formatData(),
+				viewers: 'currentUser',
+			});
+		}
+	};
 
 	if (!eventDetail) {
 		return <h1>Loading...</h1>;
@@ -30,24 +99,27 @@ function EventDetail() {
 			</div>
 			<div className='info-container'>
 				<h2>{eventDetail.name}</h2>
-				<p>Start Date: {eventDetail.dates.start.localDate}</p>
+				<p className='start'>Start Date: {eventDetail.dates.start.localDate}</p>
 				<div className='detail-btns'>
-					<button className='btn detail-btn bucket'>Add To BucketList</button>
-					<button className='btn detail-btn seen'>Add To Seen</button>
+					<button className='btn detail-btn bucket' onClick={addBucketList}>
+						Add To BucketList
+					</button>
+					<button className='btn detail-btn seen' onClick={addSeen}>
+						Add To Seen
+					</button>
 					<a target='_blank' href={eventDetail.url}>
 						<button className='btn detail-btn tickets'>View Tickets</button>
 					</a>
 				</div>
-				{
-					eventDetail.seatmap ? 
+				<h3>Seat Map</h3>
+				{eventDetail.seatmap ? (
 					<div className='seat-map'>
-					<img
-						className='event-img seat-img'
-						src={eventDetail.seatmap.staticUrl}
-						alt={`${eventDetail.name} + seat map`}></img>
-				</div> : null
-				}
-	
+						<img
+							className='event-img seat-img'
+							src={eventDetail.seatmap.staticUrl}
+							alt={`${eventDetail.name} + seat map`}></img>
+					</div>
+				) : null}
 			</div>
 		</div>
 	);
